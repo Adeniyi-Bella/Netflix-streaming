55faf28121ecfe3ad2296047f851e4a64336ec0e
Adeniyi-Bella <adeniyibella@gmail.com>
2023-03-22 18:19:35 +0100

    main component to house all other component

---
+++ pages/index.tsx
@@ -0,0 +1,107 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import Header from '@/components/Header'
+import Banner from '@/components/Banner'
+import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Row from '@/components/Row'
+
+//create a prop type for typecasting the props
+//netflix originals returns an array of type Movie
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+  // products: Product[]
+}
+
+export default function Home({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+  // products,
+}: Props) {
+  
+  return (
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
+
+        {/* //background view */}
+      <Banner netflixOriginals={netflixOriginals} />
+        
+      <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
+      </main>
+
+    </div>
+  )
+}
+
+
+// server side rendering (SSR)
+// getServerSideProps is a Next.js function that allows you to fetch data on the server before the page is rendered
+export const getServerSideProps = async () => {
+
+  // fetch data from the API and return it as props
+  // promise.all allows you to run multiple promises at the same time which is faster than running them one at a time
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+
+  //return as props so we can use them in the component
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      // products,
+    },
+  }
+}
