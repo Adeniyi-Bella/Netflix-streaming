518025df1b076752bfcc279f333d0c8d622d5f06
Adeniyi-Bella <adeniyibella@gmail.com>
2023-03-26 15:14:30 +0200

    Authentication wrapped around the app to ensure user are authenticated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AuthProvider } from "@/hooks/useAuth";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  // Ensures users are logged in or returned to the login page
+  <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>
+  );
 }
